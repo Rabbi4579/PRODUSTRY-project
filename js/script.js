362a014ae667e906071b85309ddb94664b7191dd
Rabbi4579 <fozlerabbi1120@gmail.com>
2022-06-02 22:55:25 +0600

    Produstry

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function($){
+
+    $('.bar_toggle_btn').on('click', function(){
+        $('#my_nav ul').toggleClass('menu_hide');
+        $('.logo').toggleClass('logo_left');
+    })
+
+
+
+
+
+
+})
