3b579e33c5fa5574352c92e88d7341ea0bb09cdf
Rabbi4579 <fozlerabbi1120@gmail.com>
2022-06-05 16:03:38 +0600

    Produstry

--- js/script.js
+++ js/script.js
@@ -8,6 +8,44 @@ $(function($){
 
 
 
+    //  slider js
+    
+   $('#banner').slick({
+    // autoplay:true,
+    autoplaySpeed:2000,
+    speed:1500,
+    prevArrow:'<i class="fa-solid fa-angle-left pre_arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-right next_rrow"></i>',
+
+    
+   })
+
+   $('.servi_slider').slick({
+       slidesToShow:3,
+       arrows:false,
+       autoplay:true,
+       autoplaySpeed:2000,
+       speed:1500,
+       
+       responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+              slidesToShow:2,
+            }
+          },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow:1,
+          }
+        },
+      ]
+
+       
+
+   });
+
 
 
 })
